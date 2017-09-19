3880687b8948fc7e2a25f8f7b98b3d63d8fb4327
Haktoh <hecrolfer@gmail.com>
2017-09-19 12:56:52 +0200

    aaaaaaaaaaa

--- app/src/main/java/com/example/alumno/myapp/MainActivity.java
+++ app/src/main/java/com/example/alumno/myapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void MyApp (copia)onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("Hola");
